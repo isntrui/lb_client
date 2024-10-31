20c68aadcbd8fbf1beedd03f652aca374d6f03dc
Artem Akopian <akopartem1@bk.ru>
2024-11-01 01:37:41 +0300

    im updating macos so i want to save it

--- composeApp/src/commonMain/kotlin/ru/isntrui/lb/client/Theme.kt
+++ composeApp/src/commonMain/kotlin/ru/isntrui/lb/client/Theme.kt
@@ -1,26 +1,40 @@
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import lbtool.composeapp.generated.resources.Res
+import lbtool.composeapp.generated.resources.Sector_Bold
+import lbtool.composeapp.generated.resources.Sector_ExtraBold
+import lbtool.composeapp.generated.resources.Sector_Regular
 import org.jetbrains.compose.resources.Font
-import org.jetbrains.compose.resources.FontResource
-import org.jetbrains.compose.resources.InternalResourceApi
 
-@OptIn(InternalResourceApi::class)
 @Composable
 fun CustomTheme(content: @Composable () -> Unit) {
-    val fontFamily = FontFamily(
-        Font(
-            FontResource(Res.font.),
-            FontWeight.Normal,
-            FontStyle.Normal
-        ),
+    val ff = FontFamily(
+        Font(Res.font.Sector_Regular, FontWeight.Normal, FontStyle.Normal),
+        Font(Res.font.Sector_Bold, FontWeight.Bold, FontStyle.Normal),
+        Font(Res.font.Sector_ExtraBold, FontWeight.ExtraBold, FontStyle.Normal)
     )
-
     MaterialTheme(
-        typography = MaterialTheme.typography.copy(defaultFontFamily = fontFamily),
-        content = content
+        content = content,
+        typography = Typography().copy(
+            headlineSmall = Typography().headlineSmall.copy(
+                fontFamily = ff,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Normal
+            ),
+            headlineMedium = Typography().headlineMedium.copy(
+                fontFamily = ff,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Normal
+            ),
+            headlineLarge = Typography().headlineLarge.copy(
+                fontFamily = ff,
+                fontWeight = FontWeight.ExtraBold,
+                fontStyle = FontStyle.Normal
+            ),
+        ),
     )
 }
